fd93e9232abc79fdf48713e7c83eb88c56f04462
MaplesukiD <1511608962a@gmail.com>
2024-05-30 11:38:04 +0800

    项目框架

---
+++ src/test/java/cqut/keshe3/Keshe3ApplicationTests.java
@@ -0,0 +1,13 @@
+package cqut.keshe3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Keshe3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
