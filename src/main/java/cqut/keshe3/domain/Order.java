4fe9ad08d21e9a72fa08bba912d9df273648d1ef
zuenhu <2385541509@qq.com>
2024-06-14 14:46:18 +0800

    更新订单字段

--- src/main/java/cqut/keshe3/domain/Order.java
+++ src/main/java/cqut/keshe3/domain/Order.java
@@ -1,21 +1,16 @@
 package cqut.keshe3.domain;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.Date;
 
 /**
- * @author MaplesukiD
- * @since 2024/6/2 17:13
+ * 
  * @TableName order
  */
-@TableName(value = "`order`")
+@TableName(value ="`order`")
 public class Order implements Serializable {
     /**
      * 订单id
@@ -34,6 +29,11 @@ public class Order implements Serializable {
     private Integer carId;
 
     /**
+     * 检查单id
+     */
+    private Integer checkId;
+
+    /**
      * 押金
      */
     private Integer deposit;
@@ -49,14 +49,25 @@ public class Order implements Serializable {
     private Integer days;
 
     /**
-     * 总额
+     * 总支付
      */
     private BigDecimal total;
 
     /**
      * 创建时间
      */
+    @TableField(fill = FieldFill.INSERT)
     private LocalDateTime createTime;
+
+    /**
+     * 0-进行中 1-等待检查单 2-订单完成
+     */
+    private Integer status;
+
+    /**
+     * 扣除押金（详情见检查单）
+     */
+    private Integer deductionDeposit;
 
     @TableField(exist = false)
     private static final long serialVersionUID = 1L;
@@ -104,6 +115,20 @@ public class Order implements Serializable {
     }
 
     /**
+     * 检查单id
+     */
+    public Integer getCheckId() {
+        return checkId;
+    }
+
+    /**
+     * 检查单id
+     */
+    public void setCheckId(Integer checkId) {
+        this.checkId = checkId;
+    }
+
+    /**
      * 押金
      */
     public Integer getDeposit() {
@@ -146,14 +171,14 @@ public class Order implements Serializable {
     }
 
     /**
-     * 总额
+     * 总支付
      */
     public BigDecimal getTotal() {
         return total;
     }
 
     /**
-     * 总额
+     * 总支付
      */
     public void setTotal(BigDecimal total) {
         this.total = total;
@@ -173,6 +198,34 @@ public class Order implements Serializable {
         this.createTime = createTime;
     }
 
+    /**
+     * 0-进行中 1-等待检查单 2-订单完成
+     */
+    public Integer getStatus() {
+        return status;
+    }
+
+    /**
+     * 0-进行中 1-等待检查单 2-订单完成
+     */
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    /**
+     * 扣除押金（详情见检查单）
+     */
+    public Integer getDeductionDeposit() {
+        return deductionDeposit;
+    }
+
+    /**
+     * 扣除押金（详情见检查单）
+     */
+    public void setDeductionDeposit(Integer deductionDeposit) {
+        this.deductionDeposit = deductionDeposit;
+    }
+
     @Override
     public boolean equals(Object that) {
         if (this == that) {
@@ -186,13 +239,16 @@ public class Order implements Serializable {
         }
         Order other = (Order) that;
         return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
-                && (this.getUserId() == null ? other.getUserId() == null : this.getUserId().equals(other.getUserId()))
-                && (this.getCarId() == null ? other.getCarId() == null : this.getCarId().equals(other.getCarId()))
-                && (this.getDeposit() == null ? other.getDeposit() == null : this.getDeposit().equals(other.getDeposit()))
-                && (this.getRentPrice() == null ? other.getRentPrice() == null : this.getRentPrice().equals(other.getRentPrice()))
-                && (this.getDays() == null ? other.getDays() == null : this.getDays().equals(other.getDays()))
-                && (this.getTotal() == null ? other.getTotal() == null : this.getTotal().equals(other.getTotal()))
-                && (this.getCreateTime() == null ? other.getCreateTime() == null : this.getCreateTime().equals(other.getCreateTime()));
+            && (this.getUserId() == null ? other.getUserId() == null : this.getUserId().equals(other.getUserId()))
+            && (this.getCarId() == null ? other.getCarId() == null : this.getCarId().equals(other.getCarId()))
+            && (this.getCheckId() == null ? other.getCheckId() == null : this.getCheckId().equals(other.getCheckId()))
+            && (this.getDeposit() == null ? other.getDeposit() == null : this.getDeposit().equals(other.getDeposit()))
+            && (this.getRentPrice() == null ? other.getRentPrice() == null : this.getRentPrice().equals(other.getRentPrice()))
+            && (this.getDays() == null ? other.getDays() == null : this.getDays().equals(other.getDays()))
+            && (this.getTotal() == null ? other.getTotal() == null : this.getTotal().equals(other.getTotal()))
+            && (this.getCreateTime() == null ? other.getCreateTime() == null : this.getCreateTime().equals(other.getCreateTime()))
+            && (this.getStatus() == null ? other.getStatus() == null : this.getStatus().equals(other.getStatus()))
+            && (this.getDeductionDeposit() == null ? other.getDeductionDeposit() == null : this.getDeductionDeposit().equals(other.getDeductionDeposit()));
     }
 
     @Override
@@ -202,11 +258,14 @@ public class Order implements Serializable {
         result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
         result = prime * result + ((getUserId() == null) ? 0 : getUserId().hashCode());
         result = prime * result + ((getCarId() == null) ? 0 : getCarId().hashCode());
+        result = prime * result + ((getCheckId() == null) ? 0 : getCheckId().hashCode());
         result = prime * result + ((getDeposit() == null) ? 0 : getDeposit().hashCode());
         result = prime * result + ((getRentPrice() == null) ? 0 : getRentPrice().hashCode());
         result = prime * result + ((getDays() == null) ? 0 : getDays().hashCode());
         result = prime * result + ((getTotal() == null) ? 0 : getTotal().hashCode());
         result = prime * result + ((getCreateTime() == null) ? 0 : getCreateTime().hashCode());
+        result = prime * result + ((getStatus() == null) ? 0 : getStatus().hashCode());
+        result = prime * result + ((getDeductionDeposit() == null) ? 0 : getDeductionDeposit().hashCode());
         return result;
     }
 
@@ -219,11 +278,14 @@ public class Order implements Serializable {
         sb.append(", id=").append(id);
         sb.append(", userId=").append(userId);
         sb.append(", carId=").append(carId);
+        sb.append(", checkId=").append(checkId);
         sb.append(", deposit=").append(deposit);
         sb.append(", rentPrice=").append(rentPrice);
         sb.append(", days=").append(days);
         sb.append(", total=").append(total);
         sb.append(", createTime=").append(createTime);
+        sb.append(", status=").append(status);
+        sb.append(", deductionDeposit=").append(deductionDeposit);
         sb.append(", serialVersionUID=").append(serialVersionUID);
         sb.append("]");
         return sb.toString();
--- src/main/java/cqut/keshe3/mapper/OrderMapper.java
+++ src/main/java/cqut/keshe3/mapper/OrderMapper.java
@@ -5,9 +5,9 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
-* @author MaplesukiD
+* @author Administrator
 * @description 针对表【order】的数据库操作Mapper
-* @createDate 2024-05-30 22:24:10
+* @createDate 2024-06-14 14:44:33
 * @Entity cqut.keshe3.domain.Order
 */
 @Mapper
